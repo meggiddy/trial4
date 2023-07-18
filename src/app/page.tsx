69e1c89c258ca38e22a44f4be3f21e3216067103
Maggie Muriithi <muriithinyawira13@gmail.com>
2023-07-18 12:20:26 +0300

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,54 @@
-import { GetStaticProps, GetStaticPaths, GetServerSideProps } from "next";
+import {
+  InferGetServerSidePropsType,
+  GetServerSideProps,
+  NextPage,
+} from "next";
+import { Key } from "react";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
-    </main>
-  );
-}
-export const getServerSideProps: GetServerSideProps = async (context) => {
+type User = { id: Key | null | undefined; name: string };
+type Data = { user: Record<string, string>[] };
+
+export const Home: NextPage = async () => {
   let users;
   try {
     const response = await fetch(
       "https://genuine-pegasus-34.hasura.app/v1/graphql",
       {
-        method: "GET",
+        method: "POST",
         headers: {
           "x-hasura-admin-secret":
             "lguuBuAMyVbQITmkq5oK3AbzLL17KJtrscmdAqhnqpWbcaBHwEFnmSNVJnwVL1fm",
         },
         body: JSON.stringify({
           query: `query{
-          user{
+          users{
             name
           }
         }`,
         }),
       }
     );
-    const result  = await response.json();
-    const data = result.data;
-    users = data.user;
-  } catch (e) {
-    console.log(e);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const result = await response.json();
+    const data: Data = result.data;
+    console.log(result);
+    console.log(data);
+    users = data?.users;
+  } catch (error) {
+    console.error("Fetch Error:", error);
   }
-  return {
-    props: { users },
-  };
+  console.log(users);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        {users?.map((user) => (
+          <p key={user.name}>{user.name}</p>
+        ))}
+      </div>
+    </main>
+  );
 };
+
+export default Home;
