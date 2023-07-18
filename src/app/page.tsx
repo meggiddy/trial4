d10aca58edf9798ebcb1c18ff1854e08a26084ba
Maggie Muriithi <muriithinyawira13@gmail.com>
2023-07-18 10:22:25 +0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,39 @@
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from "next";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p>HALLOOO</p>
-      </div>
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
     </main>
-  )
+  );
 }
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  let users;
+  try {
+    const response = await fetch(
+      "https://genuine-pegasus-34.hasura.app/v1/graphql",
+      {
+        method: "GET",
+        headers: {
+          "x-hasura-admin-secret":
+            "lguuBuAMyVbQITmkq5oK3AbzLL17KJtrscmdAqhnqpWbcaBHwEFnmSNVJnwVL1fm",
+        },
+        body: JSON.stringify({
+          query: `query{
+          user{
+            name
+          }
+        }`,
+        }),
+      }
+    );
+    const result  = await response.json();
+    const data = result.data;
+    users = data.user;
+  } catch (e) {
+    console.log(e);
+  }
+  return {
+    props: { users },
+  };
+};
